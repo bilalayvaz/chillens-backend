0df8d6daf876c82797ca63ef2fbddc97c82d60e5
bilalayvazoglu <bilal_ayvazoglu@hotmail.com>
2025-01-13 00:54:42 +0300

    last

--- models/AiPost.js
+++ models/AiPost.js
@@ -5,7 +5,6 @@ const aipostSchema = new mongoose.Schema(
   {
     createdContent: { type: String, required: true },
     status: { type: Boolean, default: false }, 
-    createdAt: { type: Date, default: Date.now }, 
   }
 );
 
